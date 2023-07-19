a8e3b594ad788177088f5b2712beda8da9d482ed
Juliano Josoa <36184192+julianojosoa13@users.noreply.github.com>
2023-07-19 04:12:53 +0300

    Firebase initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
 import NavigationBar from "./routes/navigation/navigation.component";
-import { Routes, Route } from "react-router-dom";
 import Shop from "./routes/shop/shop.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App=() => {
   return (
@@ -9,6 +11,7 @@ const App=() => {
       <Route path="/" element={<NavigationBar/>}>
         <Route index element={<Home/>} />
         <Route path="shop" element={<Shop/>}/>
+        <Route path="sign-in" element={<SignIn/>}/>
       </Route>
     </Routes>
   )
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -12,6 +12,9 @@ const NavigationBar = () => {
                 <CrwnLogo className="logo" />
             </Link>
             <div className="nav-links-container">
+                <Link className="nav-link" to='/sign-in'>
+                    SIGN-IN
+                </Link>
                 <Link className="nav-link" to='/shop'>
                     SHOP
                 </Link>
