46bef0f0634b3a3c7302b1d4fa931e9312f6b697
Juliano Josoa <36184192+julianojosoa13@users.noreply.github.com>
2023-07-19 03:51:31 +0300

    Mise en place des routes et de la navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from "./routes/home/home.component";
+import NavigationBar from "./routes/navigation/navigation.component";
 import { Routes, Route } from "react-router-dom";
+import Shop from "./routes/shop/shop.component";
 
 const App=() => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
+      <Route path="/" element={<NavigationBar/>}>
+        <Route index element={<Home/>} />
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
     </Routes>
   )
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -31,7 +31,7 @@ const Home = () => {
 
   return (
     <div>
-       <Categories categories={categories}/>
+        <Categories categories={categories}/>
     </div>
   );
 }
