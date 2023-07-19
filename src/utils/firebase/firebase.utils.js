e0c2710d052d7eb06d803cbbd14de1091eb9a05b
Juliano Josoa <36184192+julianojosoa13@users.noreply.github.com>
2023-07-19 21:58:35 +0300

    Firebase authentication using Google and storage of Data in Firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,14 @@ import {
     GoogleAuthProvider 
 } from 'firebase/auth';
 
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
 const firebaseConfig = {
 
     apiKey: "AIzaSyDQRM_kyzxEnq7QKtaUqm5KIZF3onNaI_s",
@@ -35,4 +43,33 @@ provider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
   
-  
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date()
+
+        try {
+            await setDoc(userDocRef,
+                {
+                    displayName,
+                    email,
+                    createdAt
+                });
+        } catch (error) {
+            console.log("error creating the user: ", error.message)
+        }
+    }
+
+    return userDocRef;
+}
