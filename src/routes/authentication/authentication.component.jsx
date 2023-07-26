545d9b082955ed728c11b80e63c8d1675d3c8131
Juliano Josoa <36184192+julianojosoa13@users.noreply.github.com>
2023-07-26 07:14:22 +0300

    Updating the README file!

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-import './sign-in.styles.scss';
+import './authentication.styles.scss';
 
 import { 
     signInWithGooglePopup,
@@ -8,10 +8,9 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import Button from "../../components/button/button.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-const SignIn = () => {
+const Authentication = () => {
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
@@ -28,4 +27,4 @@ const SignIn = () => {
     );
 }
 
-export default SignIn;
+export default Authentication;
