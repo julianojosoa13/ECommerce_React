9dcfc0313b3eeb453ba9a2599e0148a7d2f7f5f3
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-07-31 20:51:21 +0300

    Added Cart context

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
@@ -17,7 +18,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
