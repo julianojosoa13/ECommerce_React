dc266abdab7a06f9be2e394d1faf329edddcd204
josoajuliano13 <josoa.juliano.rahajaharimanga@esti.mg>
2023-07-31 16:13:33 +0300

    Added cart-dropdown basics

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -36,8 +37,9 @@ const NavigationBar = () => {
                     )
                 }
 
-                <CartIcon />               
+                <CartIcon />                
             </div>
+            <CartDropdown />
         </div>
         <Outlet/>
       </Fragment>
