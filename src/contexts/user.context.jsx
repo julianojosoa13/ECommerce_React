8c2125f9b5ea600cfd299f7f2f3e9841044cdacd
Juliano Josoa <36184192+julianojosoa13@users.noreply.github.com>
2023-08-15 11:09:15 +0300

    Using reducers instead of useState in UserContext 2

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -20,8 +20,6 @@ const INITIAL_STATE = {
 
 const userReducer = (state, action) => {
     const {type, payload} = action;
-    console.log("dispatched ")
-    console.log(action)
 
     switch(type) {
         case USER_ACTION_TYPES.SET_CURRENT_USER:
@@ -36,7 +34,7 @@ const userReducer = (state, action) => {
 
 export const UserProvider = ({ children }) => {    
     const [ {currentUser}, dispatch ] = useReducer(userReducer, INITIAL_STATE)
-    console.log(currentUser)
+
     const setCurrentUser = (user) => {
         dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
     }
