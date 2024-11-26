1cc4a6c85cc7c180f5f81c319e4fe2dff3f9e008
julianojosoa13 <josoajuliano@gmail.com>
2024-11-26 08:55:55 +0100

    custom redux logger middleware

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,36 +1,36 @@
-import './category.styles.scss';
+import "./category.styles.scss";
 
-import {  Fragment, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Fragment, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
-import { useSelector } from 'react-redux';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import ProductCard from "../../components/product-card/product-card.component";
+import { useSelector } from "react-redux";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 const Category = () => {
-    const {category} = useParams();
+  const { category } = useParams();
+  console.log("render/re-rendering category component");
 
-    const categoriesMap = useSelector(selectCategoriesMap)
-    
-    const [products, setProducts] = useState(categoriesMap[category]);
+  const categoriesMap = useSelector(selectCategoriesMap);
 
-    useEffect(()=>{
-        setProducts(categoriesMap[category]);
-    }, [category, categoriesMap])
+  const [products, setProducts] = useState(categoriesMap[category]);
 
-    return (
-        <Fragment>
-            <h2 className='category-title'>{category.toUpperCase()}</h2>
-            <div className='category-page-container'>
-                {
-                    products && products.map((product) =>{
-                        return (<ProductCard key={product.id} product={product}/>)
-                    } ) 
-                }
-            </div>
-        </Fragment>            
-    )
+  useEffect(() => {
+    console.log("effect fired calling setProducts");
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
-}
+  return (
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-page-container">
+        {products &&
+          products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
+      </div>
+    </Fragment>
+  );
+};
 
 export default Category;
