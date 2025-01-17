b0c1c02bf5f2cca0bdd64b87b4b8eeea2149bae3
julianojosoa13 <josoajuliano@gmail.com>
2025-01-17 20:53:59 +0300

    Fetching the product list form firebase using Redux Thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,31 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 
-import Category from '../category/category.component';
+import Category from "../category/category.component";
 import CategoryPreview from "../../routes/categories-preview/categories-preview.component";
 import { useEffect } from "react";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesAsync } from "../../store/categories/category.action";
 import { useDispatch } from "react-redux";
 
-
-
 const Shop = () => {
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        const getCategoriesArray = async () => {
-            const categoriesArray = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArray));
-            // console.log(categoriesArray)
-        }
-        getCategoriesArray();
-    }, []);
+  const dispatch = useDispatch();
 
-    return (
-        <Routes>
-            <Route index element={<CategoryPreview/>}/>
-            <Route path=":category" element={<Category />} />
-        </Routes>
-    )
-}
+  useEffect(() => {
+    dispatch(fetchCategoriesAsync());
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoryPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
+  );
+};
 
 export default Shop;
