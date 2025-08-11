3d3f5c7064ef398d798244d2e3fc450058a76477
julianojosoa13 <josoajuliano@gmail.com>
2025-08-11 22:25:00 +0300

    Google signin with sagas

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,80 +1,103 @@
 import { useState } from "react";
 
 import {
-    signInWithGooglePopup,
-    createUserDocumentFromAuth,
-    signInAuthUserWithEmailAndPassword
-} from '../../utils/firebase/firebase.utils';
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
-import {SignInContainer, ButtonsContainer, Subtitle } from './sign-in-form.styles.jsx';
+import {
+  SignInContainer,
+  ButtonsContainer,
+  Subtitle,
+} from "./sign-in-form.styles.jsx";
+import { googleSignInStart } from "../../store/user/user.action.js";
+import { useDispatch } from "react-redux";
 
 const defaultFormFields = {
-    email: '',
-    password: '',
-}
+  email: "",
+  password: "",
+};
 
 const SignInForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
+  const dispatch = useDispatch();
+  const [formFields, setFormFields] = useState(defaultFormFields);
 
-    const {email, password} = formFields;
+  const { email, password } = formFields;
 
-    const resetFields = () => {
-        setFormFields(defaultFormFields);
+  const resetFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { user } = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      resetFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password for email!");
+          break;
+        case "auth/user-not-found":
+          alert("No user associated with this email!");
+          break;
+        default:
+          console.log(error);
+      }
     }
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
-            resetFields();
-        } catch(error) {
-           switch(error.code) {
-                case "auth/wrong-password":
-                    alert("Incorrect password for email!");
-                    break;
-                case "auth/user-not-found":
-                    alert("No user associated with this email!");
-                    break;
-                default:
-                    console.log(error);
-           }
-        }
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
 
+  const logGoogleuser = async () => {
+    dispatch(googleSignInStart());
+  };
 
-    }
+  return (
+    <SignInContainer>
+      <Subtitle>Already have an account?</Subtitle>
+      <span>Sign-in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          required
+        />
 
-    const handleChange = (event) => {
-        const {name, value} = event.target
-        setFormFields({...formFields, [name]: value})
-    }
-
-    const logGoogleuser = async () => {
-        try {
-            await signInWithGooglePopup();
-        } catch (error) {
-            console.log(error);
-        }   
-    }
-
-    return (
-        <SignInContainer>
-            <Subtitle>Already have an account?</Subtitle>
-            <span>Sign-in with your email and password</span>
-            <form onSubmit={handleSubmit}>
-                <FormInput label='Email' type='email' name='email' value={email} onChange={handleChange} required/>
-
-                <FormInput label='Password' type='password' name='password' value={password} onChange={handleChange} required/>
-                <ButtonsContainer>
-                    <Button type='submit'>Sign In</Button>
-                    <Button buttonType='google-sign-in' type='button' onClick={logGoogleuser}>Google Sign In</Button>
-                </ButtonsContainer>
-            </form>
-
-        </SignInContainer>
-    )
-}
+        <FormInput
+          label="Password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          required
+        />
+        <ButtonsContainer>
+          <Button type="submit">Sign In</Button>
+          <Button
+            buttonType="google-sign-in"
+            type="button"
+            onClick={logGoogleuser}
+          >
+            Google Sign In
+          </Button>
+        </ButtonsContainer>
+      </form>
+    </SignInContainer>
+  );
+};
 
 export default SignInForm;
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -5,7 +5,19 @@ import { signInSuccess, signInFailed } from "./user.action";
 import {
   getCurrentUser,
   createUserDocumentFromAuth,
+  signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
+
+export function* signInWithGoogle() {
+  try {
+    const { user } = yield call(signInWithGooglePopup);
+    console.log("Google User >>> ", user);
+    yield call(getSnapshotFromUserAuth, user);
+  } catch (error) {
+    console.warn("Failed sign Google User in >> ", error);
+    yield put(signInFailed(error));
+  }
+}
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
   try {
@@ -36,10 +48,13 @@ export function* isUserAuthenticated() {
     yield put(signInFailed(error));
   }
 }
+export function* onGoogleSignInStart() {
+  yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
+}
 
 export function* onCheckUserSession() {
   yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 export function* userSagas() {
-  yield all([call(onCheckUserSession)]);
+  yield all([call(onCheckUserSession), call(onGoogleSignInStart)]);
 }
