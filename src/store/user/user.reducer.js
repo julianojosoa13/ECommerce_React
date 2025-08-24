d88f3eb83dc33444425504f47e8fb5e229230a28
julianojosoa13 <josoajuliano@gmail.com>
2025-08-24 13:28:07 +0300

    migration to userSlice (not completed)

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,3 +1,4 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { USER_ACTION_TYPES } from "./user.types";
 
 const INITIAL_STATE = {
@@ -6,56 +7,103 @@ const INITIAL_STATE = {
   error: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
+export const userSlice = createSlice({
+  name: "user",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    emailSignInStart: (state, action) => {
+      state.isLoading = true;
+    },
+    googleSignInStart: (state, action) => {
+      state.isLoading = true;
+    },
+    signOutStart: (state, action) => {
+      state.isLoading = true;
+    },
+    signInSuccess: (state, action) => {
+      (state.currentUser = action.payload), (state.isLoading = false);
+    },
+    signOutSuccess: (state, action) => {
+      (state.currentUser = null), (state.isLoading = false);
+    },
+    signInFailed: (state, action) => {
+      (state.error = action.payload), (state.isLoading = false);
+    },
+    signOutFailed: (state, action) => {
+      (state.error = action.payload), (state.isLoading = false);
+    },
+    signUpFailed: (state, action) => {
+      (state.error = action.payload), (state.isLoading = false);
+    },
+  },
+});
 
-  switch (type) {
-    case USER_ACTION_TYPES.EMAIL_SIGN_IN_START:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case USER_ACTION_TYPES.GOOGLE_SIGN_IN_START:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case USER_ACTION_TYPES.SIGN_OUT_START:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return {
-        ...state,
-        currentUser: payload,
-        isLoading: false,
-      };
-    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
-      return {
-        ...state,
-        currentUser: null,
-        isLoading: false,
-      };
-    case USER_ACTION_TYPES.SIGN_IN_FAILED:
-      return {
-        ...state,
-        error: payload,
-        isLoading: false,
-      };
-    case USER_ACTION_TYPES.SIGN_UP_FAILED:
-      return {
-        ...state,
-        error: payload,
-        isLoading: false,
-      };
-    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
-      return {
-        ...state,
-        error: payload,
-        isLoading: false,
-      };
-    default:
-      return state;
-  }
-};
+export const {
+  setCurrentUser,
+  emailSignInStart,
+  googleSignInStart,
+  signInSuccess,
+  signOutSuccess,
+  signInFailed,
+  signUpFailed,
+  signOutFailed,
+} = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
+
+// export const userReducerOld = (state = INITIAL_STATE, action) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case USER_ACTION_TYPES.EMAIL_SIGN_IN_START:
+//       return {
+//         ...state,
+//         isLoading: true,
+//       };
+//     case USER_ACTION_TYPES.GOOGLE_SIGN_IN_START:
+//       return {
+//         ...state,
+//         isLoading: true,
+//       };
+//     case USER_ACTION_TYPES.SIGN_OUT_START:
+//       return {
+//         ...state,
+//         isLoading: true,
+//       };
+//     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+//       return {
+//         ...state,
+//         currentUser: payload,
+//         isLoading: false,
+//       };
+//     case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+//       return {
+//         ...state,
+//         currentUser: null,
+//         isLoading: false,
+//       };
+//     case USER_ACTION_TYPES.SIGN_IN_FAILED:
+//       return {
+//         ...state,
+//         error: payload,
+//         isLoading: false,
+//       };
+//     case USER_ACTION_TYPES.SIGN_UP_FAILED:
+//       return {
+//         ...state,
+//         error: payload,
+//         isLoading: false,
+//       };
+//     case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+//       return {
+//         ...state,
+//         error: payload,
+//         isLoading: false,
+//       };
+//     default:
+//       return state;
+//   }
+// };
